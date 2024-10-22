919ca03685b1cf0ae1fe96ba4cccb47459685a9e
Anastasia Zef <zeff1983@gmail.com>
2024-10-22 22:04:43 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int balance1 = 150; // начальный счет
+        int amount = 1200; // сумма пополнения
+        int bonus; //количество начисляемых бонусов
+
+
+        if (amount > 1000) {
+            bonus = amount/100;
+        } else {
+            bonus = 0;
+        }
+
+        int balance2 = balance1 + amount + bonus; // итоговый счет
+
+        System.out.println("Начисленный бонус:" + bonus);
+        System.out.println("Итоговый счет:" + balance2);
+    }
+}
